7261d5e2cf10794f7e5ec00ca319d060aeb0bc57
Alehandro Rocha <alehandro.rocha@compasso.com.br>
2020-12-18 13:43:29 -0300

    ajuste de estrutura; inserido pasta assets;

--- Dass/src/pages/SignIn/index.tsx
+++ Dass/src/pages/SignIn/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import {} from 'react-native';
+import { Image } from 'react-native';
 
 import { Container } from './styles';
 
+import BackgroundImg from '../../assets/images/image2.png';
+
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={BackgroundImg} />
+    </Container>
+  );
 };
 
 export default SignIn;
